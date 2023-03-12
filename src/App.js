1e05be9c92e8dffeec62ef44c5f8d9f8b9640e3f
Bogdana <demchenkobb@gmail.com>
2023-03-12 20:45:22 +0200

    feed done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () =>  (
        <Box sx={{ backgroundColor: '#000' }}>  
           <Navbar />
           <Routes>
-            <Route pach="/" exact element={<Feed />} />
+            <Route path="/" exact element={<Feed />} />
             <Route path="/video/:id" element={<VideoDetail />} />
             <Route path="/channel/:id" element={<ChannelDetail />} />
             <Route path="/search/:searchTerm" element={<SearchFeed />} />
